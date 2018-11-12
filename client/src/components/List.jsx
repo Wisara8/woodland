902f56502e79f83bcdb7a3db39041f9f90e8e88d
wisara88 <srandall845@gmail.com>
2018-11-12 13:40:12 -0800

    abandoning dynamic rendering. 2nd order list not rendering

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -6,11 +6,28 @@ export default function List(props) {
   const name = props.items[0].name;
   const item = props.items[0].items[0].name;
   const item2 = props.items[0].items[1].name;
+
+  const newList = props.items.map((list) => {
+    const newSubList = list.items.map((subItem) => {
+       <li key={subItem.uniqueId}> {subItem.name} </li>
+    }); 
+    return (
+      <div>
+        <p> {list.name} </p>
+        <ul key={list.uniqueId}>
+          {newSubList}
+        </ul>
+      </div>
+    );
+  });
+
+
   return (
     <div>
-      <div>{name}</div>
+      {/* <div>{name}</div>
       <div>{item}</div>
-      <div>{item2}</div>
+      <div>{item2}</div> */}
+      {newList}
 
     </div>
   );
